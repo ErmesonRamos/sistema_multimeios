cdaae2f4593e97ef4268eedceb89fdf617920152
TamilaKaylany <tks2tamila520@gmail.com>
2024-09-23 06:29:45 -0300

    banco de dados

--- bd_multimeios.sql
+++ bd_multimeios.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 8.0.39, for Linux (x86_64)
+CREATE DATABASE  IF NOT EXISTS `new_sistema_multimeios` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `new_sistema_multimeios`;
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
 -- Host: localhost    Database: new_sistema_multimeios
 -- ------------------------------------------------------
--- Server version	8.0.39-0ubuntu0.22.04.1
+-- Server version	8.0.39
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +18,31 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `tb_admin`
+--
+
+DROP TABLE IF EXISTS `tb_admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_admin` (
+  `id_admin` int NOT NULL AUTO_INCREMENT,
+  `name_admin` varchar(45) NOT NULL,
+  `email_admin` varchar(255) NOT NULL,
+  `password_admin` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_admin`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_admin`
+--
+
+LOCK TABLES `tb_admin` WRITE;
+/*!40000 ALTER TABLE `tb_admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tb_book`
 --
 
@@ -27,8 +54,9 @@ CREATE TABLE `tb_book` (
   `title` varchar(90) NOT NULL,
   `gender_book` varchar(45) NOT NULL,
   `author_book` varchar(45) NOT NULL,
+  `book_cover` blob,
   PRIMARY KEY (`id_book`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,29 +69,59 @@ LOCK TABLES `tb_book` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `tb_user`
+-- Table structure for table `tb_reserve`
 --
 
-DROP TABLE IF EXISTS `tb_user`;
+DROP TABLE IF EXISTS `tb_reserve`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tb_user` (
-  `registron_user` int NOT NULL,
-  `name_user` varchar(40) NOT NULL,
-  `email_user` varchar(40) NOT NULL,
-  `password_user` varchar(40) NOT NULL,
-  `class` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`registron_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `tb_reserve` (
+  `id_reserve` int NOT NULL AUTO_INCREMENT,
+  `id_book` int NOT NULL,
+  `id_student` int NOT NULL,
+  `booking_day` date NOT NULL,
+  `return_day` date NOT NULL,
+  PRIMARY KEY (`id_reserve`),
+  KEY `id_book` (`id_book`),
+  KEY `id_student` (`id_student`),
+  CONSTRAINT `tb_reserve_ibfk_1` FOREIGN KEY (`id_book`) REFERENCES `tb_book` (`id_book`),
+  CONSTRAINT `tb_reserve_ibfk_2` FOREIGN KEY (`id_student`) REFERENCES `tb_student` (`id_student`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `tb_user`
+-- Dumping data for table `tb_reserve`
 --
 
-LOCK TABLES `tb_user` WRITE;
-/*!40000 ALTER TABLE `tb_user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tb_user` ENABLE KEYS */;
+LOCK TABLES `tb_reserve` WRITE;
+/*!40000 ALTER TABLE `tb_reserve` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_reserve` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_student`
+--
+
+DROP TABLE IF EXISTS `tb_student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_student` (
+  `id_student` int NOT NULL AUTO_INCREMENT,
+  `name_student` varchar(255) NOT NULL,
+  `email_student` varchar(255) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  `photo` blob,
+  PRIMARY KEY (`id_student`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_student`
+--
+
+LOCK TABLES `tb_student` WRITE;
+/*!40000 ALTER TABLE `tb_student` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_student` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -75,4 +133,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-17  9:10:37
+-- Dump completed on 2024-09-22 16:10:38
