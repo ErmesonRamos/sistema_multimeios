cd6e659e96f4359fff6742a522796cad124fc978
TamilaKaylany <tks2tamila520@gmail.com>
2024-09-23 06:31:46 -0300

    senha bddjmf

--- conf/conexao.php
+++ conf/conexao.php
@@ -3,7 +3,7 @@ try{
     DEFINE('HOST', 'localhost');
     DEFINE('DB', 'new_sistema_multimeios');
     DEFINE('USER', 'root');
-    DEFINE('PASS', 'whbr@520k#tk');
+    DEFINE('PASS', 'bdjmf');
 
     $conect = new PDO('mysql:host='.HOST.';dbname='.DB,USER,PASS);
     $conect -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
